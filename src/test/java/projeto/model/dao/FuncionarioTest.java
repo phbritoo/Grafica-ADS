d8004708df8d71713becb325a24bc64469a68bb6
shinvicius <shinvicius@gmail.com>
2018-11-15 18:56:10 -0300

    Primeiro teste funcionario

--- src/test/java/projeto/model/dao/FuncionarioTest.java
+++ src/test/java/projeto/model/dao/FuncionarioTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import projeto.model.Fachada.Fachada;
+import projeto.model.entity.Endereco;
+import projeto.model.entity.Funcionario;
+
 public class FuncionarioTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Funcionario funcionario = new Funcionario();
+		funcionario.setCPF("25414948070");
+		funcionario.setNome("Tito");
+		Endereco endereco = new Endereco();
+		endereco.setCEP("53130500");
+		endereco.setCidade("Recife");
+		endereco.setLogradouro("Rua da rua logradoura");
+		endereco.setBairro("bairro endereço");
+		funcionario.setEndereco(endereco);
+		
+		Fachada.getFachada().InserirFuncionario(funcionario);
 	}
 
 }
