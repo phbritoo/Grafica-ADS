81f09a80d61d066b2a1a94e2b513af95661a4aed
Gildo Neto <gildorama@gmail.com>
2018-11-15 15:20:29 -0300

    alterações em cadastroFuncionario

--- src/main/java/projeto/model/managedbean/FuncionarioManagedBean.java
+++ src/main/java/projeto/model/managedbean/FuncionarioManagedBean.java
@@ -1,27 +1,32 @@
 package projeto.model.managedbean;
-import projeto.model.entity.*;
+
+import java.util.Collection;
+
+import javax.faces.bean.ManagedBean;
+
+import projeto.model.entity.Funcionario;
 
 @ManagedBean
 public class FuncionarioManagedBean {
 
 	private Funcionario funcionario;
 	private Collection<Funcionario> aColecaoFuncionarios;
-	
+
 	public Funcionario getFuncionario() {
 		if (this.funcionario == null) {
 			this.funcionario = new Funcionario();
 		}
 		return this.funcionario;
 	}
-	
+
 	public void setFuncionario(Funcionario pFuncionario) {
 		this.funcionario = pFuncionario;
 	}
-	
-	public Collection<Funcionario> getColecaoFuncionarios(){
-		if (this.aColecaoFuncionarios == null) {
-			this.consultarGeral();
-		}
-		return aColecaoFuncionarios;
-	}
+
+//	public Collection<Funcionario> getColecaoFuncionarios() {
+//		if (this.aColecaoFuncionarios == null) {
+//			this.consultarGeral();
+//		}
+//		return aColecaoFuncionarios;
+//	}
 }
